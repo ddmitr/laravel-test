bf36b58f81b69eaa1ec8c1eba5e7a79d736194e7
Dmitry Borisov <borisov@moqod.com>
2020-05-22 13:31:43 +0300

    Eslint and prettier

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="card-header bg-blue">Example Component</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="card-header bg-blue">
+        Example Component
+      </div>
 
-            <div class="text-green-600 ml-10 pl-10">
-                I'm an example component.
-            </div>
-        </div>
+      <div class="text-green-600 ml-10 pl-10">
+        I'm an example component.
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        console.log('Component mounted.')
-    }
+  mounted() {
+    console.log('Component mounted.')
+  }
 }
 </script>
